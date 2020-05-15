5c2fabc1386fc5d80bbdf5a5f651cf16944af2f0
Cafer7 <60469116+Cafer7@users.noreply.github.com>
2020-05-15 16:15:44 -0500

    Update clasep.cpp

--- clasep.cpp
+++ clasep.cpp
@@ -2,7 +2,7 @@
 #include<string>
 #include<vector>
 
-Peliculas::Peliculas(string titulo, int año, string director, vector <string> actores, vector <string> generos){
+Peliculas::Peliculas(string titulo, int año, string director, vector <string> actores,string generos){
   t = titulo;
   a = año;
   d = director;
@@ -14,4 +14,4 @@ int Peliculas::getTitulo(){return t;}
 int Peliculas::getAnho(){return a;};
 string Peliculas::getDirector(){return d;}
 vector <string> Peliculas::getActores(){return ac;}
-vector <string> Peliculas::getGeneros(){return g;}
+string Peliculas::getGenero(){return g;}
