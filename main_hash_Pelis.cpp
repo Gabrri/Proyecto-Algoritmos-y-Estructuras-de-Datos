818a084654007f2f1869c9bd00209a5a88bd31bd
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-05-16 12:25:18 -0500

    Update and rename main.cpp to main_hash_Pelis.cpp

---
+++ main_hash_Pelis.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include "hashmap_Pelis.cpp"
+using namespace std;
+
+
+int main() {
+  // LEER TABLA EXCEL E IR GUARDANDO EN TABLA HASH
+
+  // HashMap<int> my_map;
+  // ofstream fichero;
+  // fichero.open("BaseDeDatos_Peliculas.xlsx",ios::app);
+  //
+  // ifstream lectura;
+  //   lectura.open("BaseDeDatos_Peliculas.xlsx",ios::in);
+  //   string Palabra;
+  //   cin>>Palabra;
+  //   while (lectura.good())
+  //   {
+  //       lectura>>mymap.find(Palabra);
+  //       cout << "Peliculas relcionadas" << mymap.get(Palabra):
+  //   }
+  //   lectura.close();
+
+
+
+
+  return 0;
+}
