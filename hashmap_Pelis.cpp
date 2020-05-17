3049a659def82a23c265a01400a90661ae88d05c
Gabrri <55514542+Gabrri@users.noreply.github.com>
2020-05-16 23:56:28 -0500

    Update hashmap_Pelis.cpp

--- hashmap_Pelis.cpp
+++ hashmap_Pelis.cpp
@@ -4,21 +4,9 @@
 #include "hashmap_P.hpp"
 #include<string>
 #include<vector>
+// Usamos la clase hashmap dada por el profesor adaptada a nuestro obejtivo
 
-Peliculas::Peliculas(string titulo, int año, string director, vector <string> actores,string generos){
-  t = titulo;
-  a = año;
-  d = director;
-  ac = actores;
-  g = generos
-
-}
-int Peliculas::getTitulo(){return t;}
-int Peliculas::getAnho(){return a;};
-string Peliculas::getDirector(){return d;}
-vector <string> Peliculas::getActores(){return ac;}
-string Peliculas::getGenero(){return g;}
-
+// constructor
 template <typename VT>
 HashMap<VT>::HashMap() {
   table_size = INITIAL_SIZE;
@@ -27,22 +15,37 @@ HashMap<VT>::HashMap() {
   count = 0;
 }
 
+// destructor
 template <typename VT>
 HashMap<VT>::~HashMap() {
   clear();
   delete[] table;
 }
 
+//funcion hash, sabemos que con los numeros primos no hay coliciones
+// asi que convertiremos cada key ingresado en uno primos
+
 template <typename VT>
-int HashMap<VT>::hash_fun(std::string key) {
-  int index = 0;
-  for(char c : key)
-    index += c;
-  return index % table_size;
+int HashMap<VT>::hash_fun(int key) {
+  bool primo = true;
+  int index;
+  int n = 400;
+  for(int i = 1; i < 6; i++){
+    while (400 > 0){
+      es_primo = 1;
+      for (d = 2; d < n; ++d) {
+          if (n % d == 0) {
+              for(int c: key)
+                index = n^i;
+          }
+      }
+    }
+  }
+  return index % table_size
 }
 
 template <typename VT>
-KeyValueNode<VT>* HashMap<VT>::search_bucket(int i, std::string key) {
+KeyValueNode<VT>* HashMap<VT>::search_bucket(int i, int key) {
   KeyValueNode<VT> *cursor = table[i];
   while(cursor != nullptr){
     if(cursor->key == key) return cursor;
@@ -51,8 +54,9 @@ KeyValueNode<VT>* HashMap<VT>::search_bucket(int i, std::string key) {
   return nullptr;
 }
 
+// insertar un elemento al mapa
 template <typename VT>
-void HashMap<VT>::insert(std::string key, Pelicula value) {
+void HashMap<VT>::insert(int key, VT value) {
   if(count == 2*table_size) rehash();
   int index = hash_fun(key);
   KeyValueNode<VT> *cursor = search_bucket(index, key);
@@ -68,8 +72,10 @@ void HashMap<VT>::insert(std::string key, Pelicula value) {
   }
 }
 
+// obtener el elemento a traves del key y retornando los
+// los elementos relacionados
 template <typename VT>
-VT HashMap<VT>::get(std::string key ) {
+VT HashMap<VT>::get(int key ) {
   KeyValueNode<VT> *cursor;
   int index = hash_fun(key);
   for(int i = 0; i < table_size; ++i){
@@ -79,9 +85,9 @@ VT HashMap<VT>::get(std::string key ) {
   return -1;
 }
 
-
+// verificar si un elemento esta en el map por su key
 template <typename VT>
-bool HashMap<VT>::contains(std::string key ) {
+bool HashMap<VT>::contains(int key ) {
   int index = hash_fun(key);
   KeyValueNode<VT> *cursor;
   for(int i = 0; i < table_size; ++i){
@@ -90,5 +96,7 @@ bool HashMap<VT>::contains(std::string key ) {
   }
   return false;
 }
+
+
 
 #endif
